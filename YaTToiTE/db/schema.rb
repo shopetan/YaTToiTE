83e53dd8bba28f8c46374185066ab7d32aa65f46
shopetan <s.kikuchi1120@gmail.com>
2014-09-07 23:14:18 +0900

    model名のProjectを削除 Userに変更した．同様にしてルーティングもuser配下になるように変更した．

--- YaTToiTE/db/schema.rb
+++ YaTToiTE/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907120930) do
+ActiveRecord::Schema.define(version: 20140907135916) do
 
   create_table "progects", force: true do |t|
     t.string   "YaTToiTE"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20140907120930) do
   end
 
   create_table "projects", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
